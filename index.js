1b5c2f3cc7c5cc8cce49ecfcf9228a1c463e37dc
DarkDjinn <darker.djinn@gmail.com>
2020-02-15 14:31:20 +0200

    Test change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', async (req, res) => {
-	res.send({ hi: 'there' });
+	res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
