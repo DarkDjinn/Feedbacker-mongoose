403606cfe6871b8ac091cf68fbd5442146362489
DarkDjinn <darker.djinn@gmail.com>
2020-02-26 12:52:18 +0200

    Fixed email sending

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-	app.get('/api/surveys', requireLogin, requireCredits, (req, res) => {
+	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 		const survey = new Survey({
 			title,
@@ -19,16 +19,11 @@ module.exports = app => {
 			_user: req.user.id,
 			dateSent: Date.now()
 		});
-
-		// Mailer(survey, surveyTemplate(survey));
 		const mailer = new Mailer(survey, surveyTemplate(survey));
 		mailer.send();
-		// req.user.credits -= 1;
-		// const user = await req.user.save();
-		// res.send(user);
 	});
 
 	app.post('/api/surveys/webhooks', (req, res) => {});
 
-	app.post('/api/surveys', (req, res) => {});
+	app.get('/api/surveys', (req, res) => {});
 };
